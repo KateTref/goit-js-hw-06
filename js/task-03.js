272511eba3c6091433f8bd6a04a5fd5517c3a1ff
KateTref <trefilovaekaterina408@gmail.com>
2022-09-01 20:13:27 +0200

    3 task done

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listEl = document.querySelector('.gallery');
+
+const makeItems = images.map((image) => `<li class="item"><img src = "${image.url}" width=600 alt = "${image.alt}"/></li>`)
+  .join(' ');
+
+
+
+listEl.insertAdjacentHTML('beforeend', makeItems);
