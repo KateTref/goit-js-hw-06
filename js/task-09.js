f085d65b940ed308b0a6480f120f2bca68d634dd
KateTref <trefilovaekaterina408@gmail.com>
2022-09-08 20:31:37 +0300

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  colorSpan: document.querySelector('.color'),
+  colorBtn: document.querySelector('.change-color'),
+};
+
+refs.colorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.colorSpan.textContent = getRandomHexColor();
+};
