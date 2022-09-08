5b421d59845643ff1f4c6750a015ecbb9e19a10b
KateTref <trefilovaekaterina408@gmail.com>
2022-09-08 14:47:46 +0300

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+
+    const { elements: { email, password }, } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert('Все поля должны быть заполнены!');
+        return;
+    };
+
+    const userData = {
+        email: email.value,
+        password: password.value,
+    };
+    
+    console.log(userData);
+    event.currentTarget.reset();
+};
